b8f0546dd86e0f475b235815b5b1ed6c7a97b124
Miles Acquaviva <milesacq@icloud.com>
2024-12-03 18:07:28 -0500

    Create 3-1.js

---
+++ 3-1.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+
+const input = fs.readFileSync(path.resolve(__dirname, "input.txt"), "utf8");
+const pattern = "mul\\([0-9]+,[0-9]+\\)";
+console.log(input);
+function reverseNumber(num) {
+  const reversed = parseInt(
+    Math.abs(num).toString().split("").reverse().join(""),
+  );
+  return reversed * Math.sign(num);
+}
+
+const overlappingPattern = new RegExp(pattern, "g");
+let matches = [];
+while ((match = overlappingPattern.exec(input)) !== null) {
+  matches.push(match[0]);
+}
+let total = 0;
+for (let i = 0; i < matches.length; i++) {
+  let a = 0;
+  let b = 0;
+  for (let j = 4; j < matches[i].length; j++) {
+    let num = 0;
+    while (!isNaN(matches[i][j])) {
+      num = num * 10 + Number(matches[i][j]);
+      j++;
+    }
+    if (a == 0) a = num;
+    else b = num;
+    if (b != 0) {
+      console.log(a, b);
+      total += a * b;
+    }
+  }
+}
+
+console.log(matches, total);
